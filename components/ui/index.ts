3278a654190623507051d6988d42b72d41b1129e
carlos <orellano428@gmail.com>
2023-06-02 18:36:56 -0500

    bug corregido

--- components/ui/index.ts
+++ components/ui/index.ts
@@ -2,6 +2,4 @@ export * from './NavBar';
 export * from './SideMenu'
 export * from './ImageSlideshow'
 export * from './PdfViewer' 
-export * from './HeroIllustration' 
-export * from './Container' 
-export * from './ButtonGroup' 
+
