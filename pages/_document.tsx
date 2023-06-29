9c740e17d44b8dc85c97611015d82af0f4fe98df
carlos <orellano428@gmail.com>
2023-06-28 19:14:58 -0500

    add ico

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,7 +4,7 @@ export default function Document() {
   return (
     <Html lang="es">
       <Head>
-
+        <link rel="icon" href="/logo.ico" /> {/* Cambia la ruta si tu favicon se llama de forma diferente */}
         <link href="https://fonts.googleapis.com/css2?family=Raleway:wght@200;300;400;500&display=swap" rel="stylesheet" />
 
       </Head>
