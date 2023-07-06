58da2de546b83777e090c4c9d66a9fc8553fc550
carlos <orellano428@gmail.com>
2023-07-06 12:19:24 -0500

    url mautic

--- mauticConfig.ts
+++ mauticConfig.ts
@@ -1,5 +1,5 @@
 const mauticConfig = {
-    apiUrl: 'https://mautic.groupcjc.com/api', // Ajusta la URL de acuerdo a tu instancia de Mautic
+    apiUrl: process.env.MAUTIC_URL!, // Ajusta la URL de acuerdo a tu instancia de Mautic
     username: process.env.MAUTIC_USER!, // Ajusta con tu nombre de usuario de Mautic
     password: process.env.MAUTIC_PASSWORD!, // Ajusta con tu contraseña de Mautic
   };
