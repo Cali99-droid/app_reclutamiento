5aaf36bb6eb45f13a0d3bfa102a6216219bd3900
carlos <orellano428@gmail.com>
2023-07-13 09:05:59 -0500

    debug

--- pages/admin/convocatorias/index.tsx
+++ pages/admin/convocatorias/index.tsx
@@ -29,7 +29,7 @@ interface Props {
 
 
 const ConvocatoriasPage: NextPage<Props> = ({ convos }) => {
-
+  console.log(convos)
   const [convocatorias, setConvocatorias] = useState(convos)
   const matches = useMediaQuery('(min-width:600px)');
 
