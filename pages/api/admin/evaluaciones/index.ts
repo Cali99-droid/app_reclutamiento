27fe910ccca13c44097c6286b194d02ffbd30a20
carlos <orellano428@gmail.com>
2023-08-01 18:53:21 -0500

    api ev

--- pages/api/admin/evaluaciones/index.ts
+++ pages/api/admin/evaluaciones/index.ts
@@ -1,12 +1,12 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
 
 import { prisma } from '@/server/db/client';
-import {  evaluacion_x_postulante } from '@prisma/client';
+
 import { getSession } from 'next-auth/react';
 
 type Data = 
 | { message: string }
-|{ev: evaluacion_x_postulante}
+
 
 
 export default function handler(req: NextApiRequest, res: NextApiResponse<Data>) {
@@ -28,45 +28,45 @@ const createEvaluacion = async(req: NextApiRequest, res: NextApiResponse<Data>) 
     
  
 
-  const {id, puntaje, idPos,idEv,max,idUser} = req.body
-    // const existeEv = await prisma.evaluacion_x_postulante.findMany({
-    //     where:{
-    //         convocatoria_id:parseInt(id),
-    //         AND:{
-    //             postulante_id:idPos,
-    //             evaluacion_id:idEv
-    //         }
+//   const {id, puntaje, idPos,idEv,max,idUser} = req.body
+//     // const existeEv = await prisma.evaluacion_x_postulante.findMany({
+//     //     where:{
+//     //         convocatoria_id:parseInt(id),
+//     //         AND:{
+//     //             postulante_id:idPos,
+//     //             evaluacion_id:idEv
+//     //         }
             
-    //     },
-    // })
+//     //     },
+//     // })
 
-    // if(existeEv.length>0){
-    //     return res.status(400).json({
-    //         message:'Ya tiene evaluación'
-    //     })
-    // }
+//     // if(existeEv.length>0){
+//     //     return res.status(400).json({
+//     //         message:'Ya tiene evaluación'
+//     //     })
+//     // }
 
-    try {
-        const  ev = await prisma.evaluacion_x_postulante.create({
-            data: {
-               puntaje,
-               puntaje_max:max,
-               postulante_id:idPos,
-               convocatoria_id:parseInt(id) ,
-               evaluacion_id:idEv,
-               user_id:parseInt(idUser.toString()) 
+//     try {
+//         const  ev = await prisma.evaluacion_x_postulante.create({
+//             data: {
+//                puntaje,
+//                puntaje_max:max,
+//                postulante_id:idPos,
+//                convocatoria_id:parseInt(id) ,
+//                evaluacion_id:idEv,
+//                user_id:parseInt(idUser.toString()) 
 
-            },
-        })
+//             },
+//         })
 
 
 
-        await prisma.$disconnect()
-        res.status(201).json({ev});
-    } catch (error) {
-        console.log(error);
-        await prisma.$disconnect()
-        return res.status(400).json({ message: 'Revisar logs del servidor' });
-     }
+//         await prisma.$disconnect()
+//         res.status(201).json({ev});
+//     } catch (error) {
+//         console.log(error);
+//         await prisma.$disconnect()
+//         return res.status(400).json({ message: 'Revisar logs del servidor' });
+//      }
 
 }
